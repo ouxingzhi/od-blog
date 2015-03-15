73b8a16645643a58d7e7786f91ec567b07b54a0f
ouxingzhi <ouxingzhi@vip.qq.com>
2015-03-15 23:52:56 +0800

    update

--- blog/views/foot.php
+++
@@ -1,8 +0,0 @@
-<script src="<?=$this->get('web_root','/')?>libs/jquery/jquery-1.11.2.min.js"></script>
-<?php $this->echoSection('expandlibs');?>
-<script src="<?=$this->get('web_root','/')?>libs/art/dist/dialog-min.js"></script>
-<script src="<?=$this->get('web_root','/')?>libs/common/common.js"></script>
-<script src="<?=$this->get('app_root','/')?>public/js/business.js"></script>
-<?php $this->echoSection('scripts');?>
-</body>
-</html>
--- test/index.php
+++
@@ -1,7 +0,0 @@
-<meta charset="utf8"/>
-<?php
-
-$str = "
-                　　英文原文：10ThingsGooglePlayDoesBetterthantheAppStore　　在本文中，HourTracker的制作者分享了他对于GooglePlay和AppStore两者的看法。它们在面对程序员时所采取的立场和态度。固然AppStore为他带来的收入是Android环境下的好几倍，但是他相信如果Apple能够从Google身上学到下面的十点内容，那么它将经营的更加出色。...";
-
-echo preg_replace("/\s+|u3000/ism","",$str);
