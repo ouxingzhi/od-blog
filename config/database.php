bcd23e266b0fe04e2d205cac25c1b57e3f6d85f1
ouxingzhi <ouxingzhi@vip.qq.com>
2015-03-16 00:05:30 +0800

    update config

--- config/database.php
+++ config/database.php
@@ -1,9 +1,22 @@
 <?php
+    
+    if(defined('MOPAAS_MYSQL24013_NAME')){
+        $database = MOPAAS_MYSQL24013_NAME;
+        $host = MOPAAS_MYSQL24013_HOST;
+        $user = MOPAAS_MYSQL24013_USERNAME;
+        $pass = MOPAAS_MYSQL24013_PASSWORD;
+    }else{
+        $database = 'od_blog';
+        $host = 'localhost';
+        $user = 'root';
+        $pass = '422302';
+    }
+
 	return array(
 		'mysql'=>array(
-			'host' => 'localhost',
-			'user' => 'root',
-			'password' => '422302',
-			'database' => 'od_blog'
+			'host' => $host,
+			'user' => $user,
+			'password' => $pass,
+			'database' => $database
 		)
 	);
