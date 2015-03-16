83c0cea22d6669194016ba71c10744c4f0ee1145
ouxingzhi@vip.qq.com <ouxingzhi@vip.qq.com@922f7d4f-421e-4ef0-a3df-3541b6bcc474>
2015-03-16 11:47:05 +0000

    update
    git-svn-id: https://svn.sinaapp.com/odcoder/1@51 922f7d4f-421e-4ef0-a3df-3541b6bcc474

--- blog/layouts/layout.php
+++ blog/layouts/layout.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <?php $this->insert($this->getViewName());?>
-<script src="http://libs.baidu.com/jquery/1.9.0/jquery.js"></script>
+<script src="http://libs.baidu.com/jquery/1.9.0/jquery.min.js"></script>
 <?php $this->echoSection('expandlibs');?>
 <script src="<?=$this->get('web_root','/')?>libs/art/dist/dialog-min.js"></script>
 <script src="<?=$this->get('web_root','/')?>libs/common/common.js"></script>
